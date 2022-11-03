56625dec4239a68aff56eb57a0997a6b88d00772
Katie Levandosky <katie@Katies-MacBook-Air.local>
2022-11-02 20:40:59 -0400

    update tests

--- townService/src/lib/PlayerProfile.test.ts
+++ townService/src/lib/PlayerProfile.test.ts
@@ -1,11 +1,36 @@
+import { mock, mockClear } from 'jest-mock-extended';
+import { TownEmitter } from '../types/CoveyTownSocket';
+import Player from './Player';
+import PlayerProfile from './PlayerProfile';
+
 describe('PlayerProfile', () => {
+  const username = 'username';
+  const townEmitter = mock<TownEmitter>();
+  const password = 'password';
+  let player: Player;
+  let profile: PlayerProfile;
+  beforeEach(() => {
+    mockClear(townEmitter);
+    player = new Player(username, townEmitter);
+    profile = new PlayerProfile(player, password);
+  });
   describe('username property', () => {
-    it('returns correct username', () => {});
-    it('sets to given username', () => {});
+    it('returns correct username', () => {
+      expect(profile.username).toEqual(username);
+    });
+    it('sets to given username', () => {
+      profile.username = 'newName';
+      expect(profile.username).toEqual('newName');
+    });
   });
   describe('password property', () => {
-    it('returns correct password', () => {});
-    it('sets to given password', () => {});
+    it('returns correct password', () => {
+      expect(profile.password).toEqual(password);
+    });
+    it('sets to given password', () => {
+      profile.password = 'hello';
+      expect(profile.password).toEqual('hello');
+    });
   });
   describe('avatar property', () => {
     it('is initially set to default avatar', () => {});
