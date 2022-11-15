4a2e0ed6fdb56c6b466513ac0e6e4e5d7d5f3427
Katie Levandosky <katie@Katies-MacBook-Air.local>
2022-11-15 13:02:02 -0500

    fix lint

--- frontend/src/TestUtils.ts
+++ frontend/src/TestUtils.ts
@@ -44,9 +44,8 @@ export type ReceivedEventListener<EventName extends ReceivedEvent> = ReservedOrU
  * Due to TS-41778 it is a pain to make this work for reserved events too (e.g. disconnect), so this will only work for our user-defined events,
  * but this is probably OK anyway because those are what we most want to test! https://github.com/microsoft/TypeScript/issues/41778
  */
-export type ReceivedEventParameter<
-  EventName extends EventNames<ServerToClientEvents>
-> = EventParams<ServerToClientEvents, EventName>[0];
+export type ReceivedEventParameter<EventName extends EventNames<ServerToClientEvents>> =
+ EventParams<ServerToClientEvents, EventName>[0];
 
 /**
  * Given a mocked CoveyTownSocket, return the first event listener that was registered for a given event
@@ -63,7 +62,7 @@ export function getEventListener<Ev extends ReceivedEvent>(
   if (ret) {
     const param = ret[1];
     if (param) {
-      return (param as unknown) as ReservedOrUserListener<
+      return param as unknown as ReservedOrUserListener<
         SocketReservedEventsMap,
         ServerToClientEvents,
         Ev
@@ -221,7 +220,7 @@ export function getTownEventListener<Ev extends EventNames<TownEvents>>(
   if (ret) {
     const param = ret[1];
     if (param) {
-      return (param as unknown) as TownEvents[Ev];
+      return param as unknown as TownEvents[Ev];
     }
   }
   throw new Error(`No event listener found for event ${eventName}`);
